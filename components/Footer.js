61825f07dfb9b64e742fac4ca0f6276d3ce022e9
Takyira Wilson <takyirawilson@gmail.com>
2022-12-11 15:15:30 -0600

    Pre-Netlify Deployment

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,5 @@
 import html from "html-literal";
 
 export default () => html`
-  <footer></footer>
+  <!-- <footer></footer> -->
 `;
--- components/views/Home.js
+++ components/views/Home.js
@@ -1,12 +1,19 @@
 import html from "html-literal";
+import starrysky from "/assets/img/starrysky.jpg";
 
-export default () =>
+export default state =>
   html`
-    <main>
-      <div class="box"><img src="starry sky.jpg" alt="Sky" /></div>
-      <p>
+    <section id="quote">
+      <!-- <div class="middle-img"></div><img  src="${starrysky}" /></div> -->
+      <h2>
         "The secret of your future is hidden in your daily routine" -Mike
         Murdock
-      </p>
-    </main>
+      </h2>
+    </section>
+
+    <h3>
+      The weather in ${state.weather.city} is ${state.weather.description}.
+      Temperature is ${state.weather.temp}F, and it feels like
+      ${state.weather.feelsLike}F.
+    </h3>
   `;
